5701bb1eec20424ce2326308dcdf93837df28721
Ioannis Nezis <ioannis@nezis.de>
2026-03-25 21:15:34 +0100

    ENH: show toast message for the format button

--- frontend/src/buttons/format.ts
+++ frontend/src/buttons/format.ts
@@ -4,6 +4,15 @@ export function setupFormat(editor: Editor) {
   const formatButton = document.getElementById('formatButton')!;
   formatButton.addEventListener('click', () => {
     formatDocument(editor);
+    document.dispatchEvent(
+      new CustomEvent('toast', {
+        detail: {
+          type: 'success',
+          message: 'Query formatted',
+          duration: 2000,
+        },
+      })
+    );
   });
 }
 
